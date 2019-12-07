d4cc01287b54a641398a0dfe85fad890b3dc7703
jowietoong <57376455+jowietoong@users.noreply.github.com>
2019-12-07 16:31:26 +0800

    Add files via upload

---
+++ MP1.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot as plt
+def fct (n):
+
+    if n < 9:
+        return (n**2) - 7 
+    else:
+        return fct(n-10)
+
+x = []
+y = []
+
+for i in range(0,100):
+    x.append(i)
+    y.append(fct(i))
+    
+plt.stem(x, y, use_line_collection=True)
+plt.title('Graph of Function')
+plt.xlabel('X Axis') 
+plt.ylabel('Y Axis')
+plt.show()
